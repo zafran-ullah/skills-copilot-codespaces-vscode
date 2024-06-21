405cce73a1f9503d73e9083dabd7f8b7b9dd3b3d
Zafran Ullah <85598715+zafran-ullah@users.noreply.github.com>
2024-06-21 05:29:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  const skills = [
+    { name: 'HTML', level: '90%' },
+    { name: 'CSS', level: '80%' },
+    { name: 'JavaScript', level: '70%' },
+    { name: 'React', level: '60%' },
+    { name: 'NodeJS', level: '50%' },
+    { name: 'MongoDB', level: '40%' },
+  ];
+
+  return skills;
+}
